eade2a8c02f866a4a1922e5f44d8f53be195f910
victoria-tkachenko <victoria-malysheva@yandex.ru>
2021-04-08 18:28:22 +0300

    HW6

---
+++ src/main/java/Java2/Lesson6/Homework/WeatherForecast.java
@@ -0,0 +1,30 @@
+package Java2.Lesson6.Homework;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WeatherForecast {
+
+    @JsonProperty("now_dt")
+    private String currentDate;
+
+    public WeatherForecast() {
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherForecast{" +
+                "date='" + currentDate + '\'' +
+                '}';
+    }
+
+    public String getDate() {
+        return currentDate;
+    }
+
+    public void setDate(String date) {
+        this.currentDate = currentDate;
+    }
+}
+
