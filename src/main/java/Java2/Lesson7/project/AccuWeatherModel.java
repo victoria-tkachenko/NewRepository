02de64f6cad40791d6fbae3bc525b349a478db2a
victoria-tkachenko <victoria-malysheva@yandex.ru>
2021-04-11 18:47:47 +0300

    Commit HW7

--- src/main/java/Java2/Lesson7/project/AccuWeatherModel.java
+++ src/main/java/Java2/Lesson7/project/AccuWeatherModel.java
@@ -1,5 +1,6 @@
 package Java2.Lesson7.project;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -8,16 +9,22 @@ import okhttp3.Response;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
-    public class AccuWeatherModel implements WeatherModel {
+public class AccuWeatherModel implements WeatherModel {
         private static final String PROTOKOL = "http";
-        private static final String API_KEY = "vCxNl4tOT7A8xeqR2yC0U2cSAMkoN5DM";
+        private static final String API_KEY = "qxyZGUu6M73DM9nYVzxntzXmMx2AxcBd";
         private static final String BASE_HOST = "dataservice.accuweather.com";
         private static final String CURRENT_CONDITIONS_ENDPOINT = "currentconditions";
         private static final String API_V1 = "v1";
         private static final String LOCATIONS_ENDPOINT = "locations";
         private static final String CITIES_ENDPOINT = "cities";
         private static final String AUTOCOMPLETE_ENDPOINT = "autocomplete";
+
+        private static final String FORECASTS_ENDPOINT = "forecasts";
+        private static final String DAILY_ENDPOINT = "daily";
+        private static final String FIVE_DAY_ENDPOINT = "5day";
+
 
         private static final OkHttpClient okHttpClient = new OkHttpClient();
         private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -42,11 +49,39 @@ import java.io.IOException;
 
                 Response response = okHttpClient.newCall(request).execute();
                 String responseString = response.body().string();
-                System.out.println(responseString);
+//                System.out.println(responseString);
+
+                // вывод данных в читабельном виде
+               List<WeatherResponse> weatherResponse = objectMapper.readValue(responseString, new TypeReference<List<WeatherResponse>> () {
+               });
+                System.out.println(weatherResponse);
             }
 
             if (period == Period.FIVE_DAYS) {
                 //TODO: Домашнее задание со звездочкой
+                HttpUrl httpUrl = new HttpUrl.Builder()
+                        .scheme(PROTOKOL)
+                        .host(BASE_HOST)
+                        .addPathSegment(FORECASTS_ENDPOINT)
+                        .addPathSegment(API_V1)
+                        .addPathSegment(DAILY_ENDPOINT)
+                        .addPathSegment(FIVE_DAY_ENDPOINT)
+                        .addPathSegment(cityKey)
+                        .addQueryParameter("apikey", API_KEY)
+                        .build();
+
+                Request requestFiveDays = new Request.Builder()
+                        .addHeader("accept", "application/json")
+                        .url(httpUrl)
+                        .build();
+
+                Response response = okHttpClient.newCall(requestFiveDays).execute();
+                String fiveDaysResponseString = response.body().string();
+                System.out.println(fiveDaysResponseString);
+
+                String effectiveDate = objectMapper.readTree(fiveDaysResponseString).at("/Headline/EffectiveDate").asText();
+                String forecast = objectMapper.readTree(fiveDaysResponseString).at("/Headline/Text").asText();
+                System.out.println("Прогноз погоды на "+ effectiveDate + " - " + forecast);
             }
         }
 
@@ -74,6 +109,7 @@ import java.io.IOException;
 
             return cityKey;
             //TODO: Тут нужно вывести полученные данные в читабельном виде
+            // Вывела в методе getWeather для прогноза NOW
         }
     }
 
